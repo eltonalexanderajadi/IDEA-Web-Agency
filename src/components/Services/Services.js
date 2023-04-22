e915ceca313310d21eb7cb774ed776908403fe88
eltonalexanderajadi <104156686+eltonalexanderajadi@users.noreply.github.com>
2023-04-22 03:42:33 +0100

    added animation and worked on responsiveness

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import styled from 'styled-components';
 import ServicesCard from '../Cards/ServicesCard';
 import { mobile } from '../../Responsive';
 import { tablet } from "../../Responsive";
+import {motion} from 'framer-motion';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 
 const ServicesContainer = styled.div`
@@ -14,6 +17,7 @@ const ServicesContainer = styled.div`
     margin-bottom: 17rem;
     flex-direction: column;
     ${tablet({ height: "100%", marginTop: "20rem", marginBottom: "12rem" })};
+    
 `;
 
 const Split = styled.div`
@@ -27,7 +31,7 @@ const Left = styled.div`
     display: flex;
     flex-direction: column;
     position: relative;
-    ${mobile ({ padding: "0 2rem 0 2rem"})};
+    ${mobile ({ padding: "0 2rem 0 2rem", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"})};
 
 `;
 
@@ -38,7 +42,7 @@ const Right = styled.div`
     padding: 3rem;
     flex-direction: column;
     ${tablet({ padding: "3rem 12rem 3rem 6rem"})};
-    ${mobile ({ display: "flex", flexDirection: "column", padding: "1rem 2rem 1rem 2rem"})};
+    ${mobile ({ display: "flex", flexDirection: "column", padding: "1rem 2rem 1rem 2rem", alignItems: "center", justifyContent: "center"})};
 
     h5 {
     color: #000000;
@@ -51,7 +55,7 @@ const Title = styled.div`
 
     &.title {
         margin-left: 0rem;
-        ${mobile ({ textAlign: "center"})};
+       
     }
 
     &.title:before {
@@ -70,7 +74,7 @@ const Title = styled.div`
 
 const Text = styled.div`
     width: 470px;
-    ${mobile ({ textAlign: "center", width: "350px", padding: "0 3rem 0 1rem" })};
+    ${mobile ({ textAlign: "center", width: "350px" })};
 
     span {
         font-size: 2.5rem;
@@ -83,7 +87,7 @@ const Text = styled.div`
 
 const P = styled.div`
     width: 420px;
-    ${mobile ({ textAlign: "center", width: "350px", padding: "0 3rem 0 1rem"})};
+    ${mobile ({ textAlign: "center", width: "350px"})};
 
     span {
         color: var( --color-light);
@@ -111,21 +115,26 @@ const Button = styled.button`
 `;
 
 const Services = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 1000});
+      },[]);
+
   return (
-    <ServicesContainer>
+    <ServicesContainer id = "services">
         
         <Split>
                 <Left>
-                <Title className='title'> Our Services </Title>
-                    <Text>
+                <Title className='title'> <span data-aos="fade-in"> Our Services </span> </Title>
+                    <Text data-aos="slide-right">
                     <span> We Offer The Best Services </span>
                     </Text>  
                 </Left>
                 <Right>
-                    <P>
-                    <span>Choose a beautiful and minimalist web agency with values that converts more visitors that any website.</span>
+                    <P data-aos="slide-left">
+                    <span data-aos="fade-in">Choose a beautiful and minimalist web agency with values that converts more visitors that any website.</span>
                     </P>
-                    <Btn>
+                    <Btn data-aos="slide-left">
                     <Button>
                         Learn More
                     </Button>
