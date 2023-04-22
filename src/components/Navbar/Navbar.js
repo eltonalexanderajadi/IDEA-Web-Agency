15b4d735e1747c6be9a7de65fa74a2a9eb546fd4
eltonalexanderajadi <104156686+eltonalexanderajadi@users.noreply.github.com>
2023-04-22 03:44:40 +0100

    worked on the background color of the navbar on scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { mobile } from '../../Responsive';
 import { tablet } from "../../Responsive";
@@ -13,13 +13,22 @@ const Nav = styled.nav`
   justify-content: space-between;
   position: fixed;
   z-index: 2;
+  top: 0;
+  transition: background-color 0.5s ease;
+
+  &.navbar-scroll {
+    background-color: #121212;
+    color: white;
+    z-index: 3;
+  }
+
 
   .logo {
       height: 120px;
       width: 120px;
       padding: 15px 0;
       margin-left: 20px;
-      font-size: 30px;
+      font-size: 30px; 
       ${tablet({ color: "#ffffff"})};
   }
 
@@ -29,8 +38,24 @@ const Nav = styled.nav`
 
 
 const Navbar = () => {
+  const [scroll, setScroll] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 0) {
+          setScroll(true);
+        } else {
+          setScroll(false);
+        }
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
   return (
-    <Nav>
+    <Nav className= {scroll ? "navbar-scroll" : ""}>
       <div className='logo'>
         IDEA
       </div>
